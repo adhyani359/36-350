75d8ee0b560e63052f73b37113a283c44ddbc046
Anushka Dhyani <AnushkaDhyani@Anushkas-MBP.wv.cc.cmu.edu>
2018-04-17 13:08:35 -0400

    model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,13 @@ generate.data = function(n,p) {
   responses = rnorm(n)
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariances,responses,cutoff){
+  cov.lm = summary(lm(responses ~ covariates))
+  p.values = cov.lm$coefficients[,"Pr(>|t|)"]
+  p.values = p.values <= cutoff
+  ret.lm = summary(lm(responses ~ covariates[,p.values]))
+  ret.lm.p = ret.lm$coefficients[,"Pr(>|t|)"]
+  return(ret.lm.p)
+}
+
