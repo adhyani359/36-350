a3ce894081c8e8855b6609fa4a2613a5bb7958aa
Anushka Dhyani <AnushkaDhyani@Anushkas-MBP.wv.cc.cmu.edu>
2018-04-18 21:29:32 -0400

    run_simulation function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,11 +5,33 @@ generate.data = function(n,p) {
 }
 
 model_select = function(covariances,responses,cutoff){
-  cov.lm = summary(lm(responses ~ covariates))
+  cov.lm = summary(lm(responses ~ covariances))
   p.values = cov.lm$coefficients[,"Pr(>|t|)"]
   p.values = p.values <= cutoff
-  ret.lm = summary(lm(responses ~ covariates[,p.values]))
+  p.values = p.values[2:length(p.values)]
+  if (length(covariances[,p.values]) == 0) { return(c())}
+  ret.lm = summary(lm(responses ~ covariances[,p.values]))
   ret.lm.p = ret.lm$coefficients[,"Pr(>|t|)"]
   return(ret.lm.p)
 }
 
+run_simulation = function(n_trials, n, p, cutoff) {
+  p.vals = c()
+  for (i in 1:n_trials) {
+    data = generate.data(n,p)
+    result = model_select(data$covariates,data$responses,cutoff)
+    p.vals = c(p.vals,result)
+  }
+  hist(p.vals)
+}
+
+par(mfrow = c(3,3))
+run_simulation(30,100,10,0.05)
+run_simulation(30,100,20,0.05)
+run_simulation(30,100,50,0.05)
+run_simulation(30,1000,10,0.05)
+run_simulation(30,1000,20,0.05)
+run_simulation(30,1000,50,0.05)
+run_simulation(30,10000,10,0.05)
+run_simulation(30,10000,20,0.05)
+run_simulation(30,10000,50,0.05)
